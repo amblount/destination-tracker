a7321b0c2cdaac0a1f046e3738b30eacefd781d1
rhamill1 <rhamill11@gmail.com>
2016-10-24 14:21:30 -0700

    revises bad migrations

--- vagabond/db/migrate/20161024185419_create_posts.rb
+++ vagabond/db/migrate/20161024185419_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
-      t.user :belongs_to
-      t.city :belongs_to
+      t.belongs_to :user, index: true
+      t.belongs_to :city, index: true
       t.string :name
       t.text :content
 
