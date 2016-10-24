85cb49914e769da64d91e38e89184e9f33be325d
lordshadowist <shadowist@live.com>
2016-10-24 15:42:05 -0700

    Bugs

--- vagabond/db/migrate/20161024184934_create_users.rb
+++ vagabond/db/migrate/20161024184934_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.city :belongs_to
+      t.belongs_to :city, foreign_key: true
       t.string :email
       t.string :image_url
 
