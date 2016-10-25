5d767fdcc453c6c13ed27cfa8eaf7dc143ba50b4
lordshadowist <shadowist@live.com>
2016-10-25 14:36:01 -0700

    City Show

--- vagabond/app/controllers/cities_controller.rb
+++ vagabond/app/controllers/cities_controller.rb
@@ -1,13 +1,17 @@
 class CitiesController < ApplicationController
   def new
+  	@city = City.new
   end
 
   def edit
   end
 
   def index
+  	@cities = City.all
   end
 
   def show
+  	@city = City.find_by_id(params[:id])
+
   end
 end
