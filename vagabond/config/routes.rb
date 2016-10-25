bca88cb217686104e69b3315a53827934d55ce3c
Alivia Blount <aliviablount@gmail.com>
2016-10-25 16:00:17 -0700

    CRUD for posts nested under cities

--- vagabond/config/routes.rb
+++ vagabond/config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
  delete '/logout' => 'sessions#destroy'
 
  resources :cities do
-  resources :posts, only: [:new, :create]
+  resources :posts
  end
 
  # get 'cities/new'
